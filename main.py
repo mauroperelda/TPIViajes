2c98f0a947069719beafc3b2310344a65db160d3
mauroperelda <87204830+mauroperelda@users.noreply.github.com>
2024-06-29 15:02:58 -0300

    Template layout
    Se agrego frontend para el dashboard

--- main.py
+++ main.py
@@ -6,11 +6,23 @@ from routers.UserDestinos import destinos_router
 from routers.UserPaquetes import paquetes_router
 from routers.UserReservas import reservas_router
 from routers.UserUsuarios import usuarios_router
-
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 
 app = FastAPI()
 app.title = "Viajes"
 app.version = "0.0.1"
+
+# Montar la carpeta static para servir archivos estáticos
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Configurar Jinja2Templates
+templates = Jinja2Templates(directory="Templates")
+
+@app.get("/")
+async def read_dashboard(request: Request):
+    return templates.TemplateResponse("Layout.html", {"request": request})
 
 app.add_middleware(ErrorHandler)
 app.include_router(destinos_router)
